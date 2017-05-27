7f546eeeaea075d4f0ffb6b98d9387bdfc78127c
Valerijs Volinecs <Val@Valerijss-MacBook-Air.local>
2017-05-27 15:46:55 +0300

    Mock party list added.

--- bun-net/src/app/components/screens/list-screen/list-screen.component.ts
+++ bun-net/src/app/components/screens/list-screen/list-screen.component.ts
@@ -1,4 +1,10 @@
+import { Party } from './../../../party';
 import { Component, OnInit } from '@angular/core';
+
+const PARTIES: Party[] = [
+  {id: 1, name: 'John Doe', type: '1', phone: 1, email: 'john.doe@example.com', status: 1},
+  {id: 2, name: 'John Doe2', type: '2', phone: 2, email: 'john.doe2@example.com', status: 2} 
+];
 
 @Component({
   selector: 'app-list-screen',
@@ -6,10 +12,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-screen.component.css']
 })
 export class ListScreenComponent implements OnInit {
-
+  parties = PARTIES;
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
