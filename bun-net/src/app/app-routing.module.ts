35f6cacfb50447f27f93f9fdecb07203e446111a
artjoman <artjoms.v.petrovs@gmail.com>
2017-05-27 13:07:35 +0300

    Router module implemented

--- bun-net/src/app/app-routing.module.ts
+++ bun-net/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { DetailsScreenComponent } from './components/screens/details-screen/details-screen.component';
+import { LoginScreenComponent } from './components/screens/login-screen/login-screen.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginScreenComponent },
+  { path: 'details', component: DetailsScreenComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
